cf4beecca00eb100c603aac6c81f59785d17428c
Your Name <you@example.com>
2018-03-13 07:59:02 +0100

    zefzfzefze

--- src/projet_Serveur/travailleur.go
+++ src/projet_Serveur/travailleur.go
@@ -4,12 +4,14 @@ import (
   "fmt"
   "strconv"
 )
+// "time"
 
 func Travailler(workChan chan int, availaibleWorkers chan chan int) {
-  for  {
+  for {
     var taskNumber int = <-workChan
     for i := 0; i < taskNumber; i++ {
       fmt.Println(strconv.Itoa(i))
+      // time.Sleep(1000000)
     }
     fmt.Println("Travailleur : J'ai travaillé "+strconv.Itoa(taskNumber)+" fois")
     availaibleWorkers <- workChan
